45a9f6c485223055b83d3c3285960fd838bdaa5d
Eli Treuherz <eli.treuherz@cgi.com>
2017-03-20 10:01:47 +0000

    Backup script complete

--- scripts/scripts/backup.sh
+++ scripts/scripts/backup.sh
@@ -2,4 +2,4 @@ dest=$1
 timestamp=$(date -u +%Y%m%dT%H%M%SZ)
 prev_time=$(ls -F $1 | grep -P '^\d{8}T\d{6}Z/$' | sed 's/\///' | tail -n1)
 
-rsync -azhvCF --partial --stats --log-file="$1/logs/$timestamp.log" --dry-run --progress --link-dest="$1/$prev_time" ~/ "$1/$timestamp"
+rsync -azhvCF --partial --stats --log-file="$1/logs/$timestamp.log" --out-format="%t %''b %f" --progress --link-dest="$1/$prev_time" ~/ "$1/$timestamp"
